71b5b4eb1a63632c1ad3eb7f26ed81007bd216d8
akmphuljhb024 <akmphuljhb024@student.wethinkcode.co.za>
2025-05-30 07:57:17 +0200

    redirecting

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
 
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
     return "Hello"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+#Allow for redirecting instead of a 404!,takes you back to the homepage.
+#E.g "url/admin" ,this will take you to the home page 
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run()
