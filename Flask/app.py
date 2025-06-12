953f2c9044efb605b99325c24243a5a053e5118c
akmphuljhb024 <akmphuljhb024@student.wethinkcode.co.za>
2025-06-12 14:12:06 +0200

    sessions

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,9 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request,session
 
 app = Flask(__name__)
+
+# Secret key for decripting
+app.secret_key = "akhona"
 
 @app.route("/")
 def home():
@@ -17,13 +20,21 @@ def admin():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
+
+        # Sessions this store data about the user in the for of a dic
+        session["user"] = user
+
         return redirect(url_for("user",usr=user))
     else:
         return render_template("login.html")
 
 @app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1></h1>"
+    else:
+        return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
