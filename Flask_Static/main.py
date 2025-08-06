c74ba7a0c6ad40b567b6ac56a1fcecb3ff80d970
akmphuljhb024 <akmphuljhb024@student.wethinkcode.co.za>
2025-08-06 14:49:25 +0200

    testing

--- Flask_Static/main.py
+++ Flask_Static/main.py
@@ -4,5 +4,11 @@ from second import second
 app = Flask(__name__)
 app.register_blueprint(second, url_prefix="/admin")
 
+# @app.route("/home")
+@app.route("/")
+def home():
+    return "<h1>Tests</h1>"
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
